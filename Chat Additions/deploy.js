a5481d3e588cb8ff2616ab5c7eb4d73ff7579d9a
Bibbytube <megafaqqq@gmail.com>
2013-10-26 00:20:17 +0200

    add inputhistory.js

--- Chat Additions/deploy.js
+++ Chat Additions/deploy.js
@@ -5,5 +5,9 @@ $.getScript('https://raw.github.com/Bibbytube/Instasynch/master/Chat%20Additions
 //messagefilter
 $.getScript('https://raw.github.com/Bibbytube/Instasynch/master/Chat%20Additions/Messagefilter/messagefilter.js');
 
-//messagefilter
+//inputhistory
+$.getScript('https://raw.github.com/Bibbytube/Instasynch/master/Chat%20Additions/Input%20History/inputhistory.js');
+
+//wallcounter
 $.getScript('https://raw.github.com/Bibbytube/Instasynch/master/Playlist%20Additions/wallcounter.js');
+
