3375b1922f0ce87e1afed8fb4f877ea22170c713
Bibbytube <megafaqqq@gmail.com>
2013-10-25 19:50:51 +0200

    added InstaSynch's new license to the source

--- Chat Additions/Autocomplete/autocomplete.js
+++ Chat Additions/Autocomplete/autocomplete.js
@@ -1,11 +1,35 @@
+/*
+    <InstaSynch - Watch Videos with friends.>
+    Copyright (C) 2013  InstaSynch
+
+    <Faqqq- Modified InstaSynch client code>
+    Copyright (C) 2013  Faqqq
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    http://opensource.org/licenses/GPL-3.0
+*/
+
+
+
 function loadAutoComplete() {
 
     //wait until we got a connection to the server
     //needs to be replaced with something better
+
     if (messages < 3) {
-        setTimeout(function () {
-            loadAutoComplete();
-        }, 100);
+        setTimeout(function () {loadAutoComplete();}, 100);
         return;
     }
 
@@ -72,7 +96,7 @@ function loadAutoComplete() {
             commands : 
         null;
 
-    //add the jquery autcomplete widget to the input field
+    //add the jquery autcomplete widget to InstaSynch's input field
     $("#chat input").autocomplete({
 
         //this function is needed so that the string will be matched from the beginng so /a won't find /pekaface
