e4b4ac0cde67562e170c673a3488f8d415a6a193
Bibbytube <megafaqqq@gmail.com>
2013-10-25 18:40:41 +0200

    update deploy.js

--- Chat Additions/deploy.js
+++ Chat Additions/deploy.js
@@ -8,9 +8,8 @@
 // @grant       none
 // ==/UserScript==
 
-with(unsafeWindow) {
-	//autocomplete
-   	$.getScript('https://github.com/Bibbytube/Instasynch/tree/master/Chat%20Additions/Autocomplete/autocomplete.js')
-   	//messagefilter
-   	$.getScript('https://github.com/Bibbytube/Instasynch/tree/master/Chat%20Additions/Messagefilter/Messagefilter.js')
-}
+//autocomplete
+$.getScript('https://github.com/Bibbytube/Instasynch/tree/master/Chat%20Additions/Autocomplete/autocomplete.js')
+ 
+//messagefilter
+$.getScript('https://github.com/Bibbytube/Instasynch/tree/master/Chat%20Additions/Messagefilter/Messagefilter.js')
