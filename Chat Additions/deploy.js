cdec3742456b3496b777ab0528a25813dc1f35a7
Bibbytube <megafaqqq@gmail.com>
2013-10-25 18:45:43 +0200

    update deploy.js add loadAll.js

--- Chat Additions/deploy.js
+++ Chat Additions/deploy.js
@@ -1,12 +1,3 @@
-// ==UserScript==
-// @name        Instasynch Addons
-// @namespace   Bibby
-// @description autocompletes emotes and or commands
-// @include     http://*.instasynch.com/rooms/*
-// @include     http://instasynch.com/rooms/*
-// @version     1
-// @grant       none
-// ==/UserScript==
 
 //autocomplete
 $.getScript('https://raw.github.com/Bibbytube/Instasynch/master/Chat%20Additions/Autocomplete/autocomplete.js')
---
+++ Chat Additions/loadAll.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        Instasynch Addons
+// @namespace   Bibby
+// @description autocompletes emotes and or commands
+// @include     http://*.instasynch.com/rooms/*
+// @include     http://instasynch.com/rooms/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+
+$.getScript('https://raw.github.com/Bibbytube/Instasynch/master/Chat%20Additions/deploy.js')
