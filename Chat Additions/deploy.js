9d59d573507e8956bfa208491c90a1f33e615185
Bibbytube <megafaqqq@gmail.com>
2013-10-26 16:29:40 +0200

    added mousewheelvolumecontrol.js to deploy.js

--- Chat Additions/deploy.js
+++ Chat Additions/deploy.js
@@ -11,3 +11,5 @@ $.getScript('https://raw.github.com/Bibbytube/Instasynch/master/Chat%20Additions
 //wallcounter
 $.getScript('https://raw.github.com/Bibbytube/Instasynch/master/Playlist%20Additions/Wallcounter/wallcounter.js');
 
+//Mousewheel Volumecontrol
+$.getScript('https://raw.github.com/Bibbytube/Instasynch/master/Player%20Additions/Mousewheel%20Volumecontrol/mousewheelvolumecontrol.js');
