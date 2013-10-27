1500085c1e17f293cf532f60e150cb7eb15321b2
Bibbytube <megafaqqq@gmail.com>
2013-10-27 23:18:07 +0100

    update deploy for tests

--- Chat Additions/deploy.js
+++ Chat Additions/deploy.js
@@ -1,18 +1,18 @@
 
 //messagefilter
-$.getScript('https://raw.github.com/Bibbytube/Instasynch/tree/test/Chat%20Additions/Messagefilter/messagefilter.js');
+$.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Chat%20Additions/Messagefilter/messagefilter.js');
 
 //autocomplete
-$.getScript('https://raw.github.com/Bibbytube/Instasynch/tree/test/Chat%20Additions/Autocomplete/autocomplete.js');
+$.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Chat%20Additions/Autocomplete/autocomplete.js');
 
 //inputhistory
-$.getScript('https://raw.github.com/Bibbytube/Instasynch/tree/test/Chat%20Additions/Input%20History/inputhistory.js');
+$.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Chat%20Additions/Input%20History/inputhistory.js');
 
 //name autocomplete
-$.getScript('https://raw.github.com/Bibbytube/Instasynch/tree/test/Chat%20Additions/Name%20Autocomplete/nameautocomplete.js');
+$.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Chat%20Additions/Name%20Autocomplete/nameautocomplete.js');
 
 //wallcounter
-$.getScript('https://raw.github.com/Bibbytube/Instasynch/tree/test/Playlist%20Additions/Wallcounter/wallcounter.js');
+$.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Playlist%20Additions/Wallcounter/wallcounter.js');
 
 //Mousewheel Volumecontrol
-$.getScript('https://raw.github.com/Bibbytube/Instasynch/tree/test/Player%20Additions/Mousewheel%20Volumecontrol/mousewheelvolumecontrol.js');
+$.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Player%20Additions/Mousewheel%20Volumecontrol/mousewheelvolumecontrol.js');
