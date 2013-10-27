b2b52b003c2edf1a79ad78f3ab3a982ce6d12943
Bibbytube <megafaqqq@gmail.com>
2013-10-27 22:31:21 +0100

    Update deploy.js

--- Chat Additions/deploy.js
+++ Chat Additions/deploy.js
@@ -1,9 +1,8 @@
-
 //messagefilter
 $.getScript('https://raw.github.com/Bibbytube/Instasynch/master/Chat%20Additions/Messagefilter/messagefilter.js');
 
 //autocomplete
-$.getScript('https://raw.github.com/Bibbytube/Instasynch/master/Chat%20Additions/Autocomplete/autocomplete.js');
+//$.getScript('https://raw.github.com/Bibbytube/Instasynch/master/Chat%20Additions/Autocomplete/autocomplete.js');
 
 //inputhistory
 $.getScript('https://raw.github.com/Bibbytube/Instasynch/master/Chat%20Additions/Input%20History/inputhistory.js');
