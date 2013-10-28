74e3d40c88edd5d6324fd6ee2eedd7c2f3327582
Bibbytube <megafaqqq@gmail.com>
2013-10-28 20:17:47 +0100

    using dropbox links so I can test without commits

--- Chat Additions/deploy.js
+++ Chat Additions/deploy.js
@@ -1,18 +1,18 @@
 
 //messagefilter
-$.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Chat%20Additions/Messagefilter/messagefilter.js');
+$.getScript('https://dl.dropboxusercontent.com/1/view/en9aeqotf2nuex2/github/instasynch/chat%20additions/Messagefilter/messagefilter.js');
 
 //autocomplete
-$.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Chat%20Additions/Autocomplete/autocomplete.js');
+$.getScript('https://dl.dropboxusercontent.com/1/view/8jd9x58xfd811w8/github/instasynch/chat%20additions/Autocomplete/autocomplete.js');
 
 //inputhistory
-$.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Chat%20Additions/Input%20History/inputhistory.js');
+$.getScript('https://dl.dropboxusercontent.com/1/view/wd0a3j5pyseyptl/github/instasynch/chat%20additions/Input%20History/inputhistory.js');
 
 //name autocomplete
-$.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Chat%20Additions/Name%20Autocomplete/nameautocomplete.js');
+$.getScript('https://dl.dropboxusercontent.com/1/view/g0zqbai0sr9646b/github/instasynch/chat%20additions/Name%20Autocomplete/nameautocomplete.js');
 
 //wallcounter
-$.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Playlist%20Additions/Wallcounter/wallcounter.js');
+$.getScript('https://dl.dropboxusercontent.com/1/view/qfil2a2jpkk7raq/github/instasynch/playlist%20additions/Wallcounter/wallcounter.js');
 
 //Mousewheel Volumecontrol
-$.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Player%20Additions/Mousewheel%20Volumecontrol/mousewheelvolumecontrol.js');
+$.getScript('https://dl.dropboxusercontent.com/1/view/swh5m00f4z6zwg9/github/instasynch/player%20additions/Mousewheel%20Volumecontrol/mousewheelvolumecontrol.js');
