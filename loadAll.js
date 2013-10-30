603f9bc8cceb31e2f242236718be1f50a1a30b20
Bibbytube <megafaqqq@gmail.com>
2013-10-30 20:18:04 +0100

    fixing

---
+++ loadAll.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Instasynch Addons
+// @namespace   Bibby
+// @description autocompletes emotes and or commands
+// @include     http://*.instasynch.com/rooms/*
+// @include     http://instasynch.com/rooms/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+var oldOnload = window.onload;
+window.onload=function onload(){
+    if(oldOnload){
+	   oldOnload();
+    }
+    $.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Chat%20Additions/deploy.js');
+};
