271c2be7b15e97766cecd3158326ea14866bdf98
Bibbytube <megafaqqq@gmail.com>
2013-11-01 23:13:28 +0100

    disable scrolling for now

--- deploy.js
+++ deploy.js
@@ -34,7 +34,7 @@ $.when(
     $.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Chat%20Additions/OnClickKickBan/OnClickKickBan.js'),
 
     //Autoscroll Fix
-    $.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Chat%20Additions/Autoscroll%20Fix/AutoscrollFix.js'),
+    //$.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Chat%20Additions/Autoscroll%20Fix/AutoscrollFix.js'),
 
     //wallcounter
     $.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Playlist%20Additions/Wallcounter/wallcounter.js'),
