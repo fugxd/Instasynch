6de06047a01ecb3b63f73669871c5789644c30ff
Bibbytube <megafaqqq@gmail.com>
2013-11-01 02:18:06 +0100

    merge fix

---
+++ Chat Additions/deploy.js
@@ -0,0 +1,50 @@
+
+var afterConnectFunctions = [];
+var beforeConnectFunctions = [];
+
+function afterConnect(){
+    if (messages < 3) {
+        setTimeout(function () {afterConnect();}, 100);
+        return;
+    }
+
+    for(var i = 0; i< afterConnectFunctions.length;i++){
+            afterConnectFunctions[i]();
+    }
+}
+function beforeConnect(){
+    for(var i = 0; i< beforeConnectFunctions.length;i++){
+            beforeConnectFunctions[i]();
+    }
+}
+$.when(
+    $.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Chat%20Additions/Messagefilter/messagefilter.js'),
+    
+    //autocomplete
+    $.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Chat%20Additions/Autocomplete/autocomplete.js'),
+    
+    //inputhistory
+    $.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Chat%20Additions/Input%20History/inputhistory.js'),
+    
+    //name autocomplete
+    $.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Chat%20Additions/Name%20Autocomplete/nameautocomplete.js'),
+    
+    //OnClick kick&ban
+    $.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Chat%20Additions/OnClickKickBan/OnClickKickBan.js'),
+
+    //wallcounter
+    $.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Playlist%20Additions/Wallcounter/wallcounter.js'),
+    
+    //Mousewheel Volumecontrol
+    $.getScript('https://raw.github.com/Bibbytube/Instasynch/test/Player%20Additions/Mousewheel%20Volumecontrol/mousewheelvolumecontrol.js'),
+    
+    //Description replacement
+    $.getScript('https://raw.github.com/Bibbytube/Instasynch/test/General%20Additions/Description.js'),
+
+    $.Deferred(function( deferred ){
+        $( deferred.resolve );
+    })
+).done(function(){
+        beforeConnect();
+        afterConnect();
+});
