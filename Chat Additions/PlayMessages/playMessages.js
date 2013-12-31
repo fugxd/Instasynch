76565b001a1cd9a74e61301d929cd10ac0bd6c7a
fugxd <fugxd1@gmail.com>
2013-12-31 02:44:53 +0100

    Two settings added for the following features:  - playMessages, will show text in the chat window for every time the playing video changes.  - bigPlaylist, changes the list-based playlist to a tabel based playlist, with video thumbnails and a cleaner look.

---
+++ Chat Additions/PlayMessages/playMessages.js
@@ -0,0 +1,48 @@
+/*
+    Copyright (C) 2013 fugXD
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    http://opensource.org/licenses/GPL-3.0
+*/
+
+function loadPlayMessages(){
+    //load settings
+    playMessages = settings.get('playMessages','false');
+    
+    //add the command
+    commands.set('addOnSettings',"playMessages",togglePlayMessages);
+    
+    // Overwriting Adduser
+    var oldPlayVideo = playVideo;
+
+    playVideo = function(vidinfo, time, playing) {
+        // Only if blackname or mod
+        if (playMessages){
+            indexOfVid = getVideoIndex(vidinfo);
+            title = playlist[indexOfVid].title;
+            addMessage('', 'Now playing: ' + title, '','hashtext'); 
+        }
+        oldPlayVideo(vidinfo, time, playing);
+    };
+}
+
+var playMessages = false;
+
+function togglePlayMessages(){
+    playMessages = !playMessages;
+    settings.set('playMessages',playMessages);
+}
+
+afterConnectFunctions.push(loadPlayMessages);
